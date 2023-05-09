6cc9f3a88d9f39eb72e9d8b4d08d6a99ab0bf6b1
JoaoBett <joao.gouveia.bettencourt@gmail.com>
2023-05-09 10:02:13 +0100

    fix: dir content

--- PHP/WheatherAPI/index.php
+++ PHP/WheatherAPI/index.php
@@ -7,15 +7,19 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Temp API</title>
+    <link rel="stylesheet" href="css/style.css">
+    <title>Wheather API</title>
 </head>
 <body>
     <header>
-
+        <div>
+        <h1 class="Title">Wheather API</h1>
+        <img src="images/icon_wheatherjpg.jpg" alt="WheatherICONs">
+        </div>
     </header>
     
     <main>
-
+        <--!  --->
 
     </main>
 
