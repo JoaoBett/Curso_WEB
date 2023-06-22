d03fa28a0f0a853a053e42f0a9bbb099ec51e0dc
JoaoBett <joao.gouveia.bettencourt@gmail.com>
2023-06-22 17:48:15 +0100

    feat: exercicio 1/2

---
+++ PHP/exercicios/exercicio2.php
@@ -0,0 +1,18 @@
+<?php
+    $nome = "john";
+    $apelido = "doe";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>
+        <?php echo "$nome $apelido" ?>
+    </h1>
+</body>
+</html>
