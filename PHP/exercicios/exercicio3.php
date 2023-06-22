4bcf01bb5fb54a81d8d70b873270879ed2a4b41e
JoaoBett <joao.gouveia.bettencourt@gmail.com>
2023-06-22 17:50:51 +0100

    feat: exercicio 3

---
+++ PHP/exercicios/exercicio3.php
@@ -0,0 +1,20 @@
+<?php
+    $num1 = 0;
+    $num2 = 0;
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>
+        <?php
+            $num3 = $num1 + $num2;
+            echo "$num1 + $num2 = $num3";
+        ?>
+    </h1>
+</body>
+</html>
